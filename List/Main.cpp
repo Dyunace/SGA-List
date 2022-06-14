c6d8a4555e4868ae2c892de78ffe61af416165b7
tmddn5924 <tmddn5924@naver.com>
2022-06-14 11:17:51 +0900

    // List v0.2

--- List/Main.cpp
+++ List/Main.cpp
@@ -34,12 +34,9 @@ int main(void)
 	Output(NumberList);
 	cout << endl;
 
+	cout << "erase" << endl;
 	erase(NumberList, 3);
 	Output(NumberList);
-
-	cout << endl;
-
-
 
 	return 0;
 }
@@ -61,15 +58,9 @@ void AddObject(List* _Next, const int& _Value)
 
 void Output(List* _Next)
 {
-	cout << "ShowValue _Value : " << _Next->Value << endl;
-	cout << "ShowValue _Back : " << _Next->Back << endl;
-	cout << "ShowValue & : " << &_Next << endl;
-	
 	if (_Next->Back)
 		cout << "ShowValue _Back->Value : " << _Next->Back->Value << endl;
 	else cout << "nullptr" << endl;
-
-	cout << endl;
 
 	if (_Next->Back != nullptr)
 		Output(_Next->Back);
@@ -97,37 +88,19 @@ void erase(List* _Next, const int _Where)
 	if (_Where == 0)
 	{
 		List* Temp = _Next;
-
-		cout << "Temp->Value : " << Temp->Value << endl;
-		cout << "Temp->Back : " << Temp->Back << endl;
-		cout << "Next->Back : " << _Next->Back << endl;
-
 		_Next = _Next->Back;
-
-		cout << "Next(Next->Back) : " << _Next << endl;
 
 		delete Temp;
 		Temp = nullptr;
 	}
-	/*
 	else if (_Where == 1)
 	{
 		List* Temp = _Next->Back;
-		cout << Temp << endl << endl;
-		cout << Temp->Back << endl;
-		cout << _Next->Back->Back << endl;
-		cout << &Temp->Back << endl;
-		cout << &_Next->Back->Back << endl << endl;
 		_Next->Back = _Next->Back->Back;
-
-		cout << _Next->Back << endl;
-		cout << &_Next->Back << endl << endl;
-
 		
 		delete Temp;
 		Temp = nullptr;
 	}
-	*/
 	else
 		erase(_Next->Back, _Where - 1);
 }
